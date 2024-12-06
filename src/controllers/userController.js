ad0d32733df48fb9934360b3cee262a689845825
AsdhyM <asdhyme@gmail.com>
2024-12-06 19:16:55 +1000

    fix: changed indexjs to help with testing later

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// Create an instance of a route
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+// Export the router for the app to use
+module.exports = {
+    router
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const express = require("express");
+const {app} = require("./server.js");
 
-// Make an instance of the Express system to configure it
-const app = express();
-
-// GET localhost:3000/
-app.get("/", (request, response) => {
-    response.json({
-        message:"Hello World!"
-    });
-});
-
-app.post("/", (request, response) => {
-    response.json({
-        message: "POST request received!"
-    });
-});
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on localhost:${PORT}`);
+    console.log("Server is running on port " + PORT);
 });
