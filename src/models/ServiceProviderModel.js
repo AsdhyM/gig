b52655718029b7f9e706c052bcdaf51c2e7c72e0
AsdhyM <asdhyme@gmail.com>
2024-12-11 17:27:11 +1000

    feat: created service provider model

---
+++ src/models/ServiceProviderModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+// Service Provider Schema
+const ServiceProviderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 3,
+        trim: true,
+        unique: true,
+        match: /.+\@.+\..+/
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        trim: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    tradeSkill: {
+        type: String,
+        required: true,
+    },
+    experience: {
+        type: String,
+        required: true,
+    },
+    about: {
+        type: String,
+        required: true,
+    },
+    availability: {
+        type: Boolean,
+        required: true
+    },
+    date: {
+        type: Number,
+        required: true
+    },
+    booking: {
+        type: Object,
+        default: {}
+    }
+},{minimize:false})
+
+// Model based on the schema
+const ServiceProviderModel = mongoose.model("ServiceProvider", ServiceProviderSchema);
+
+// Exporting the model
+module.exports = {
+    ServiceProviderModel
+}
