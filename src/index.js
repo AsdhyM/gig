0bd9a6ac28c2efb5f2ecdc7d2ed88affddf46fdd
AsdhyM <asdhyme@gmail.com>
2024-12-11 16:20:18 +1000

    feat: connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const {app} = require("./server.js");
 
 require("dotenv").config();
 
+const {app} = require("./server.js");
+const { dbConnect } = require("./utils/database.js");
+
+
+
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+
+    await dbConnect();
+
     console.log("Server is running on port http://localhost:" + PORT);
 });
