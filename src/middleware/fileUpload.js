f450a0683e237da22a0fabc31b5a77eb8db89a72
AsdhyM <asdhyme@gmail.com>
2024-12-11 22:40:32 +1000

    feat: added limitations to upload files

--- src/middleware/fileUpload.js
+++ src/middleware/fileUpload.js
@@ -6,9 +6,22 @@ const storage = multer.diskStorage({
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.round(Math.random() * 1E9))
         callback(null, file.originalname)
     }
-})
+});
 
-const upload = multer({storage: storage})
+const fileFilter = function(request, file, callback) {
+    const approvedTypes = ["pdf", "jpeg", "png"];
+    if (approvedTypes.includes(file.mimetype)) {
+        callback(null, true);
+    } else {
+        callback(new Error("Only PDF, JPEG, and PNG files are accepted."))
+    }
+};
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 },
+    fileFilter
+});
 
 module.exports = {
     upload
